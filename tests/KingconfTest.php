bcc71691afac1a28f53a91beb3edabd6e7391cc5
Marijn Ophorst <marijn@sensimedia.nl>
2015-12-02 07:06:42 +0100

    add tests

---
+++ tests/KingconfTest.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Kingconf\Tests;
+
+use PHPUnit_Framework_TestCase;
+use Kingconf\Config;
+
+class KingconfTest extends PHPUnit_Framework_TestCase
+{
+    private function config($env)
+    {
+        return function () use ($env) {
+            return $env;
+        };
+    }
+
+    private function runtests($config)
+    {
+        $config = dirname(__FILE__)."/$config";
+        $kingconf = new Config($config);
+        $this->assertEquals('bar', $kingconf['foo']);
+    }
+
+    public function testJson()
+    {
+        $this->runtests('json.json');
+    }
+
+    public function testIni()
+    {
+        $this->runtests('ini.ini');
+    }
+
+    public function testPhp()
+    {
+        $this->runtests('php.php');
+    }
+
+    public function testYaml()
+    {
+        $this->runtests('yaml.yml');
+    }
+
+    public function testXml()
+    {
+        $this->runtests('xml.xml');
+    }
+
+    public function testAll()
+    {
+        $kingconf = new Config(
+            dirname(__FILE__).'/overwrite.json',
+            dirname(__FILE__).'/json.json',
+            dirname(__FILE__).'/ini.ini',
+            dirname(__FILE__).'/php.php',
+            dirname(__FILE__).'/yaml.yml',
+            dirname(__FILE__).'/xml.xml'
+        );
+        $this->assertEquals('baz', $kingconf['foo']);
+    }
+}
+
